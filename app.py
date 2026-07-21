f4bb4d82e229bfea82fa8b87624b5a60b1bcac92
kritiikkaaa <kritikama58@gmail.com>
2026-07-21 19:32:53 +0530

    Update app.py for Vercel

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 import pickle
+import os
 
 app = Flask(__name__)
 
+# Base directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Load the trained model and vectorizer
-with open("fake_news_model.pkl", "rb") as model_file:
+with open(os.path.join(BASE_DIR, "fake_news_model.pkl"), "rb") as model_file:
     model = pickle.load(model_file)
 
-with open("vectorizer.pkl", "rb") as vec_file:
+with open(os.path.join(BASE_DIR, "vectorizer.pkl"), "rb") as vec_file:
     vectorizer = pickle.load(vec_file)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -24,10 +29,13 @@ def home():
             transformed_input = vectorizer.transform([news_text])
             prediction = model.predict(transformed_input)[0]
 
-    return render_template("index.html", prediction=prediction, news_text=news_text, submitted=submitted)
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        news_text=news_text,
+        submitted=submitted
+    )
 
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
